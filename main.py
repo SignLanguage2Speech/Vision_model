8988f92d53a1bbefee36c871a5a673af5511e3a2
Maplewarrior <michaelharborg@gmail.com>
2023-01-05 17:46:47 +0100

    .

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
 import os
 import torch
 from model import S3D
+from utils.load_weigths import load_model_weights
 
-
-weight_filename = 'S3D_kinetics400.pt'
-
-def load_model_weights(n_classes):
+def main():
+    n_classes = 400
     model = S3D(n_classes)
-    weights = torch.load(os.path.join('weights', weight_filename))
-    sd = model.state_dict()
-
-    for name, param in weights.items():
-        # fix naming issues in kinetics state dict
-        if "module" in name:
-            name = name.strip('module.')
-            name = name.replace('track', 'tracked')
-        if name in sd:
-            if param.size() == sd[name].size():
-                sd[name].copy_(param)
-            else:
-                print(f"name: {name}\npre-trained size {param.size()}\nInit size: {sd[name].size()}")
-        else:
-            print(f"Param {name} not in state dict")
-            
-    return model
-
-m = load_model_weights(100)
+    model = load_model_weights(model, 'S3D_kinetics400.pt')
+    
---
+++ utils/load_weigths.py
@@ -0,0 +1,22 @@
+import os
+import torch
+
+def load_model_weights(model, weight_filename = 'S3D_kinetics400.pt'):
+    
+    weights = torch.load(os.path.join('weights', weight_filename))
+    sd = model.state_dict()
+
+    for name, param in weights.items():
+        # fix naming issues in kinetics state dict
+        if "module" in name:
+            name = name.strip('module.')
+            name = name.replace('track', 'tracked')
+        if name in sd:
+            if param.size() == sd[name].size():
+                sd[name].copy_(param)
+            else:
+                print(f"name: {name}\npre-trained size {param.size()}\nInit size: {sd[name].size()}")
+        else:
+            print(f"Param {name} not in state dict")
+            
+    return model
