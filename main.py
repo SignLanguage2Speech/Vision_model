90efa22fa6b995ba00c3fcba07f797b272dcb896
Maplewarrior <michaelharborg@gmail.com>
2023-02-13 22:36:06 +0100

    further training code for WLASL

--- main.py
+++ main.py
@@ -1,95 +1,105 @@
 import os
 import numpy as np
 import pandas as pd
+import time
+
 import torch
 import torch.nn as nn
-import torch.utils.data as data
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from utils.WLASLDataset import WLASLDataset
 from model import S3D
 from utils.load_weigths import load_model_weights
-from PIL import Image
+
 
 #import subprocess
 #subprocess.call('pip list')
 ### opencv-python is here but cv2 gives importerror...
 
-def transform_rgb(snippet):
-  #print(f"Number of images being transformed: {len(snippet)}")
-  #print(f"Original image size: {snippet[0].shape}")
-  ''' stack & normalization '''
-  snippet = np.concatenate(snippet, axis=-1)
-  snippet = torch.from_numpy(snippet).permute(2, 0, 1).contiguous().float()
-  snippet = snippet.mul_(2.).sub_(255).div(255)
-  out = snippet.view(1,-1,3,snippet.size(1),snippet.size(2)).permute(0,2,1,3,4)
-  #print(f"Post transformation size: {out.size()}")
-  return out
 
-def transform_gray(snippet):
-  #print(f"Number of images being transformed: {len(snippet)}")
-  #print(f"Original image size: {snippet[0].shape}")
-  ''' stack & normalization '''
-  snippet = np.concatenate(snippet, axis=-1)
-  snippet = torch.from_numpy(snippet).permute(2, 0, 1).contiguous().float()
-  snippet = snippet.mul_(2.).sub_(255).div(255)
-  out = snippet.view(1,-1,1,snippet.size(1),snippet.size(2)).permute(0,2,1,3,4)
-  #print(f"Post transformation size: {out.size()}")
-  return out
-
-images = {video_id : [] for video_id in video_names}
-for v in video_names[:1]:
-  video_path = os.path.join(img_folder, v)
-  #imgs = [np.expand_dims(np.asarray(Image.open(os.path.join(video_path, f)).convert('L')),2) for f in os.listdir(video_path)[1:]]
-  #print(imgs[0].shape)
-  images[v] = transform_gray([np.expand_dims(np.asarray(Image.open(os.path.join(video_path, f)).convert('L')),2) for f in os.listdir(video_path)])
-  #images[v] = transform([np.asarray(Image.open(os.path.join(video_path, f))) for f in os.listdir(video_path)[1:]])
-
-class WLASLDataset(data.Dataset):
-  def __init__(self, df, img_folder, gray_scale=False):
-    super().__init__()
-    self.df = df
-    self.img_folder = img_folder
-    self.video_names = os.listdir(self.img_folder)
-    self.gray_scale = gray_scale
-
-  def __getitem__(self, idx):
-    video_path = os.path.join(self.img_folder, self.video_names[idx])
+class cfg:
+  def __init__(self):
+    self.start_epoch = 0
+    self.n_epochs = 80
+    self.checkpoint = None
+    self.batch_size = 6 # per GPU (they have 56 haha)
+    self.lr = 0.1
+    self.momentum = 0.9
+    self.weight_decay = 5e-4
+    self.num_workers = 4
+    self.print_freq = 100
+    self.multipleGPUs = False
+    # for data augmentation
+    self.crop_size=224
+    self.temporal_length = 64
     
-    if self.gray_scale:
-      images = transform_gray([np.expand_dims(np.asarray(Image.open(os.path.join(video_path, f)).convert('L')),2) for f in os.listdir(video_path)])
-    else:
-      images = transform_rgb([np.asarray(Image.open(os.path.join(video_path, f))) for f in os.listdir(video_path)])
+    
 
-    trg = self.df['label']
-
-    return images, trg
-  
-  def __len__(self):
-    return len(self.video_names)
 
 def main():
-  # load data
+  CFG = cfg()
+  ############## load data ##############
   df = pd.read_csv('data/WLASL/WLASL_labels.csv')
   img_folder = os.path.join(os.getcwd(), 'data/WLASL/WLASL_images')
-  video_names = os.listdir(img_folder)
-  images = {video_id : [] for video_id in video_names}
-  # load model
+  WLASL = WLASLDataset(df, img_folder, gray_scale=False)
+  
+  ############## load model ##############
   n_classes = len(set(df['gloss'])) #2000
   model = S3D(n_classes)
-  model = load_model_weights(model, 'S3D_kinetics400.pt')
+  if CFG.checkpoint is None:
+    model = load_model_weights(model, 'S3D_kinetics400.pt')
+
+  ############## initialize dataloader ##############
+  if CFG.multipleGPUs:
+    dataloader = DataLoader(WLASL, batch_size=CFG.batch_size, 
+                                   shuffle=True,
+                                   num_workers=CFG.num_workers,
+                                   pin_memory=True)
+    model = torch.nn.parallel.DistributedDataParallel(model)
   
-  for v in video_names[:10]:
-    video_path = os.path.join(img_folder, v)
-    images[v] = transform([np.asarray(Image.open(os.path.join(video_path, f))) for f in os.listdir(video_path)])
+  else:
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"Device: {device}")
+    dataloader = DataLoader(WLASL, batch_size=CFG.batch_size, 
+                                   shuffle=True,
+                                   num_workers=CFG.num_workers)
+    model.cuda()                   
+      
+  criterion = nn.CrossEntropyLoss().cuda()
+  optimizer = optim.SGD(model.parameters(),
+                        CFG.lr,
+                        weight_decay=CFG.weight_decay,
+                        momentum=CFG.momentum)
   
-  out = model.forward(images[video_names[0]])
-  print(torch.softmax(out, dim=1).size())
-  print(torch.sum(torch.softmax(out, dim=1)))
-  pred = torch.argmin(torch.log_softmax(out, dim=1))
+  for epoch in range(CFG.start_epoch, CFG.n_epochs):
+    # adjust learning rate
+    #adjust_lr(optimizer, CFG.lr_step, epoch)
+    # run train loop
+    train(model, dataloader, optimizer, criterion, CFG)
   
-  criterion = nn.CrossEntropyLoss()
 
-  #loss = criterion()
+def train(model, dataloader, optimizer, criterion, CFG):
+  losses = []
+  model.train()
+  start = time.time()
+  for i, (ipt, trg) in enumerate(dataloader):
+    trg=trg.cuda()
+    ipt_var = torch.autograd.Variable(ipt)
+    trg_var = torch.autograd.Variable(trg)
+
+    out = model(ipt_var)[0]
+    preds = nn.Softmax(out)
+    loss = criterion(preds, trg_var)
+    losses.append(loss)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if i % CFG.print_freq == 0:
+      print(f"Iter: {i}/{len(dataloader)}\nAvg loss: {np.mean(losses)}\nTime: {np.round(start, 2)/60} min")
 
 
-
-#if __name__ == '__main__':
-  #main()
+if __name__ == '__main__':
+  main()
---
+++ utils/WLASLDataset.py
@@ -0,0 +1,58 @@
+import os
+import numpy as np
+import torch 
+from PIL import Image
+import torch.utils.data as data
+
+def transform_rgb(snippet):
+  #print(f"Number of images being transformed: {len(snippet)}")
+  #print(f"Original image size: {snippet[0].shape}")
+  ''' stack & normalization '''
+  snippet = np.concatenate(snippet, axis=-1)
+  snippet = torch.from_numpy(snippet).permute(2, 0, 1).contiguous().float()
+  snippet = snippet.mul_(2.).sub_(255).div(255)
+  out = snippet.view(1,-1,3,snippet.size(1),snippet.size(2)).permute(0,2,1,3,4)
+  #print(f"Post transformation size: {out.size()}")
+  return out
+
+def transform_gray(snippet):
+  #print(f"Number of images being transformed: {len(snippet)}")
+  #print(f"Original image size: {snippet[0].shape}")
+  ''' stack & normalization '''
+  snippet = np.concatenate(snippet, axis=-1)
+  snippet = torch.from_numpy(snippet).permute(2, 0, 1).contiguous().float()
+  snippet = snippet.mul_(2.).sub_(255).div(255)
+  out = snippet.view(1,-1,1,snippet.size(1),snippet.size(2)).permute(0,2,1,3,4)
+  #print(f"Post transformation size: {out.size()}")
+  return out
+
+def revert_transform_rgb(clip):
+  clip = clip.squeeze(0).permute(1, 2, 3, 0)
+  clip = clip.contiguous().view(1, -1, clip.size(1), clip.size(2)).squeeze(0)
+  clip = clip.mul_(255).add_(255).div(2)
+  clip = clip.view(-1, clip.size(1), clip.size(2), 3)
+  return clip.numpy()
+
+
+class WLASLDataset(data.Dataset):
+  def __init__(self, df, img_folder, grayscale=False):
+    super().__init__()
+    self.df = df
+    self.img_folder = img_folder
+    self.video_names = os.listdir(self.img_folder)
+    self.grayscale = grayscale
+
+  def __getitem__(self, idx):
+    video_path = os.path.join(self.img_folder, self.video_names[idx])
+    
+    if self.grayscale:
+      images = transform_gray([np.expand_dims(np.asarray(Image.open(os.path.join(video_path, f)).convert('L')),2) for f in os.listdir(video_path)])
+    else:
+      images = transform_rgb([np.asarray(Image.open(os.path.join(video_path, f))) for f in os.listdir(video_path)])
+
+    trg = self.df['gloss'][idx]
+    #trg = self.df['label'][idx]
+    return images, trg
+  
+  def __len__(self):
+    return len(self.video_names)
