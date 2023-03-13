6969c0511bf738464ec266139f28b1ce386463f7
MapleWarrior <michaelharborg@gmail.com>
2023-03-13 11:04:36 +0100

    Update data_exploration.py

--- PHOENIX/data_exploration.py
+++ PHOENIX/data_exploration.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
-
+"""
 annotations_path = '/work3/s204138/bach-data/PHOENIX/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/annotations/manual'
 features_path = '/work3/s204138/bach-data/PHOENIX/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/fullFrame-210x260px'
 
@@ -20,18 +20,22 @@ annotations = list(train['orth'])# + list(test['orth']) + list(val['orth'])
 annotations = list(sorted(set([word for sent in annotations for word in sent.replace(chars,'').split(' ')])))
 print(f"Train vocab size: {len(annotations)}")
 
-#### Data preprocessing ####
-
-
-
-##### Dataset class for Phoenix #####
-import torch
-import torchvision
-from torch.utils import data
-import numpy as np
-import pandas as pd
-import subprocess
+"""
 from PIL import Image
+import torch
+
+### Testing Dataset class locally ###
+ipt_dir = os.path.join(os.getcwd(), 'PHOENIX\\sample_data')
+
+def load_imgs(ipt_dir):
+  image_names = os.listdir(ipt_dir)
+  N = len(image_names)
+  imgs = np.empty((N, 260, 210, 3))
+
+  for i in range(N):
+    imgs[i,:,:,:] = np.asarray(Image.open(os.path.join(ipt_dir, image_names[i])))
+  
+  return imgs
 
 def transform_rgb(video):
   ''' stack & normalization '''
@@ -48,11 +52,47 @@ def revert_transform_rgb(clip):
   clip = clip.view(-1, clip.size(1), clip.size(2), 3)
   return clip.numpy()
 
-############# Data augmentations #############
+
+video_names = os.listdir(ipt_dir)
+imgs = load_imgs(os.path.join(ipt_dir, video_names[0]))
+print(f"loaded size {imgs.shape}")
+Image.fromarray(imgs[0].astype(np.uint8)).show()
+
+#imgs = transform_rgb(imgs)
+#imgs = revert_transform_rgb(imgs)
+#Image.fromarray(imgs[0].astype(np.uint8)).show()
+
+
+Upsample = torch.nn.Upsample(size=(298, 240), scale_factor=None, mode='bilinear', align_corners=None, recompute_scale_factor=None)
+
+imgs = torch.from_numpy(imgs).double().permute(0, 3, 1, 2).contiguous()
+print(f"PRE CONV SIZE {imgs.size()}")
+
+imgs = Upsample(imgs)
+
+
+imgs2 = imgs.permute(0, 2, 3, 1)
+
+print("POST PERMUTE SIZE: ", imgs2.size())
+imgs2 = transform_rgb(imgs2.numpy())
+imgs2 = revert_transform_rgb(imgs2)
+Image.fromarray(imgs2[0].astype(np.uint8)).show()
+
+imgs2= transform_rgb(imgs2)
+print(f"Post transform {imgs2.size()}")
+
+"""
 class DataAugmentations:
   def __init__(self):
+    self.H_upsample = 298
+    self.W_upsample = 240
     self.H_out = 224
     self.W_out = 224
+
+  def UpsamplePixels(self, imgs: np.ndarray):
+    Upsample = torch.nn.Upsample(size=(self.H_upsample, self.W_upsample), scale_factor=None, mode='bilinear', align_corners=None, recompute_scale_factor=None)
+    imgs = Upsample(torch.from_numpy(imgs).double().permute(0, 3, 1, 2).contiguous()) # upsample and place color channel as dim 1
+    return imgs.permute(0, 2, 3, 1) # return and revert dim changes
 
   # flip all images in video horizontally with 50% probability
   def HorizontalFlip(self, imgs):
@@ -75,111 +115,11 @@ class DataAugmentations:
   def RandomRotation(self, imgs):
     rotate = torchvision.transforms.RandomRotation(5, expand=False, fill=0.0, interpolation=torchvision.transforms.functional.InterpolationMode.BILINEAR)
     return rotate(imgs)
-
-def upsample(images, seq_len):
-  images_org = images.detach().clone() # create a clone of original input
-  if seq_len / images.size(1) >= 2: # check if image needs to be duplicated
-    repeats = int(np.floor(seq_len / images.size(1)) - 1) # number of concats
-    for _ in range(repeats):
-      images = torch.cat((images, images_org), dim=1) # concatenate images temporally
-  
-  if seq_len > images.size(1):
-    start_idx = np.random.randint(0, images_org.size(1) - (seq_len - images.size(1))) 
-    stop_idx = start_idx + (seq_len - images.size(1))
-    images = torch.cat((images, images_org[:, start_idx:stop_idx]), dim=1)
-
-  return images
-
-def downsample(images, seq_len):
-  start_idx = np.random.randint(0, images.size(1) - seq_len)
-  stop_idx = start_idx + seq_len
-  return images[:, start_idx:stop_idx]
-
-# fps=25 is default for WLASL
-def video2array(vname, input_dir, fps=25):
-  H = W = 210, 260 # default dims for Phoenix
-  name, ext = os.path.splitext(vname)
-  video_path = os.path.join(input_dir, vname)
-  # out = []
-  get_no_of_frames = ['ffprobe', '-v', 'error', '-select_streams', 'v:0', '-count_packets', '-show_entries', 'stream=nb_read_packets', '-of', 'csv=p=0',video_path]
-  n_frames = int(subprocess.check_output(get_no_of_frames))
-  # pdb.set_trace()
-  out = np.zeros((n_frames,H,W,3))
-  cmd = f'ffmpeg -i {video_path} -f rawvideo -pix_fmt rgb24 -threads 1 -r {fps} pipe:'
-  pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, bufsize=10**8, stderr=subprocess.DEVNULL)
-
-  # while True:
-  for i in range(n_frames):
-    buffer = pipe.stdout.read(H*W*3)
-    if len(buffer) != H*W*3:
-      break
-    # out.append(np.frombuffer(buffer, np.uint8).reshape(H, W, 3))
-    out[i,:,:,:] = np.frombuffer(buffer, dtype=np.uint8).reshape(H, W, 3)
-    # pdb.set_trace()
-  pipe.stdout.close()
-  pipe.wait()
-  # pdb.set_trace()
-  return out
-
-def load_imgs(ipt_dir):
-  image_names = os.listdir(ipt_dir)
-  N = len(image_names)
-  imgs = np.empty((N, 210, 260, 3))
-
-  for i in range(N):
-    imgs[i,:,:,:] = np.asarray(Image.open(os.path.join(ipt_dir, image_names[i])))
-  
-  return imgs
-  
+"""
 
 
-class PhoenixDataset(data.Dataset):
-    def __init__(self, df, ipt_dir, vocab_size, seq_len=64, train=True):
-        super().__init__()
-        self.df = df
-        self.ipt_dir = ipt_dir
-        self.seq_len = seq_len
-        self.train = train
-        self.vocab_size = vocab_size
-        self.video_folders = list(self.df['id'])
 
-    def __getitem__(self, idx):
+#imgs = TransposeConv(imgs)
+#print(imgs.shape)
+### train augmentations ###
 
-        ### Assumes that within a sample (id column in df) there is only one folder named '1' ###
-        # TODO Check that this holds!
-        image_folder = os.path.join(self.ipt_dir, self.video_folders[idx])
-        images = load_imgs(image_folder)
-
-        if self.train:
-          images = transform_rgb(images) # convert to tensor, reshape and normalize 
-          # resize images
-          # take a crop in range [0.7, 1.]
-          # frame rate augmentation
-
-          # check if we need to upsample
-          if self.seq_len > images.size(1): 
-            images = upsample(images, self.seq_len)
-          # check if we need to downsample
-          elif self.seq_len < images.size(1):
-            images = downsample(images, self.seq_len)
-        
-        else:
-           images = transform_rgb(images)
-           images = self.DataAugmentation.HorizontalFlip(images)
-           images = self.DataAugmentations.CenterCrop(images)
-
-        # make a one-hot vector for target class
-        sent_len = len(self.df.iloc[idx]['annotation'].split(' '))
-
-        trg = torch.zeros((sent_len, self.vocab_size)) # 2000 unique words
-        for i in range(sent_len):
-           trg[i]
-        gloss_idx = self.df.iloc[idx]['label']
-        trg[gloss_idx] = 1
-        
-        return images, trg
-
-    
-    def __len__(self):
-        return len(self.df)
-    
