6210675fc51e45057081092766e63081dac2e294
Maplewarrior <michaelharborg@gmail.com>
2023-04-10 13:57:37 +0200

    updated visualencoder

--- models/HeadNetwork.py
+++ models/HeadNetwork.py
@@ -1,17 +1,20 @@
 import torch.nn as nn
-
+from models.utils import PositionalEncoding, MaskedNorm
 
 class HeadNetwork(nn.Module):
     def __init__(self, n_classes, input_size, hidden_size, ff_size, ff_kernel_size, residual_connection=True) -> None:
         super().__init__()
         self.residual_connection = residual_connection
-        self.head1 = nn.Sequential(
-                    nn.Linear(input_size, hidden_size),
-                    MaskedNorm(num_features=hidden_size),
-                    nn.ReLU(),
-                    PositionalEncoding(hidden_size),
-                    nn.Droput(p=0.1)
-                    )
+        self.layer_norm1 = nn.LayerNorm(input_size, eps=1e-06)
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.bn1 = MaskedNorm(num_features=hidden_size, norm_type='batch')
+        self.relu1 = nn.ReLU()
+
+        self.PE = PositionalEncoding(d_model=hidden_size, N=10000)
+        self.dropout1 = nn.Dropout(0.1)
+        self.pe = nn.Sequential(
+                    PositionalEncoding(d_model=hidden_size, N=10000),
+                    nn.Dropout(p=0.1))
         
         self.temp_conv_block = nn.Sequential(
                                 nn.Conv1d(hidden_size, ff_size, kernel_size=3, stride=1, padding='same'),
@@ -20,23 +23,39 @@ class HeadNetwork(nn.Module):
                                 nn.Conv1d(ff_size, hidden_size, kernel_size=3, stride=1, padding='same'),
                                 nn.Dropout(p=0.1))
 
-        self.layer_norm = nn.LayerNorm(hidden_size, eps=1e-06)
-
+        
+        self.layer_norm2 = nn.LayerNorm(hidden_size, eps=1e-06)
+        self.layer_norm3 = nn.LayerNorm(hidden_size, eps=1e-06)
         self.translation_layer = nn.Linear(hidden_size, n_classes)
     
-    def load_weights(self, model, ckpt='WLASL'):
+    def load_weights(self, model, ckpt):
         ckpts = ['phoenix', 'wlasl', 'kinetics', 'how2sign']
         assert(ckpt.lower() in ckpts, print(f"{ckpt} is not a valid checkpoint!\n Valid ones are:\n{ckpts}"))
         print(f"Loading weights for {ckpt}")
         
 
-    def forward(self, x):
-        # x = [N x T x 832]
-        x2 = self.head1(x)
-        if self.residual_connection:
-            x2 = self.temp_conv_block(x2) + x
-        else:
-            x2 = self.temp_conv_block(x2)
-        logits = self.translation_layer(x2)
+    def forward(self, x, mask):
+        #Input: x = [N x T/4 x 832]
 
+        # Head
+        x = self.layer_norm1(x)
+        x = self.fc1(x)
+        x = self.bn1(x, mask)
+        x = self.relu1(x)
+
+        x = self.PE(x)
+        x = self.dropout1(x)
+
+        # temporal convolutional block
+        print("BEFORE CONV", x.size())
+        if self.residual_connection:
+            x = self.temp_conv_block(self.layer_norm2(x).transpose(1, 2)).transpose(1, 2) + x 
+        else:
+            x = self.temp_conv_block(self.layer_norm2(x).transpose(1, 2)).transpose(1, 2)
+        print("AFTER CONV", x.size())
+        x = self.layer_norm3(x)
+        # gloss translation layer
+        logits = self.translation_layer(x)
+        
+        print("Logits: ", logits.size())
         return logits
