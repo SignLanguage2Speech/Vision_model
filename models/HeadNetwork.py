b105d0cfb88638f9cf4811507456a517d9659ffa
Maplewarrior <michaelharborg@gmail.com>
2023-04-10 11:10:00 +0200

    push

--- models/HeadNetwork.py
+++ models/HeadNetwork.py
@@ -2,9 +2,9 @@ import torch.nn as nn
 
 
 class HeadNetwork(nn.Module):
-    def __init__(self, n_classes, input_size, hidden_size, ff_size, ff_kernel_size) -> None:
+    def __init__(self, n_classes, input_size, hidden_size, ff_size, ff_kernel_size, residual_connection=True) -> None:
         super().__init__()
-
+        self.residual_connection = residual_connection
         self.head1 = nn.Sequential(
                     nn.Linear(input_size, hidden_size),
                     MaskedNorm(num_features=hidden_size),
@@ -19,10 +19,7 @@ class HeadNetwork(nn.Module):
                                 nn.Dropout(p=0.1),
                                 nn.Conv1d(ff_size, hidden_size, kernel_size=3, stride=1, padding='same'),
                                 nn.Dropout(p=0.1))
-        
-        
-        
-        
+
         self.layer_norm = nn.LayerNorm(hidden_size, eps=1e-06)
 
         self.translation_layer = nn.Linear(hidden_size, n_classes)
@@ -35,8 +32,11 @@ class HeadNetwork(nn.Module):
 
     def forward(self, x):
         # x = [N x T x 832]
-        x = self.head1(x)
-        x = self.temp_conv_block(x)
-        x = self.translation_layer(x)
+        x2 = self.head1(x)
+        if self.residual_connection:
+            x2 = self.temp_conv_block(x2) + x
+        else:
+            x2 = self.temp_conv_block(x2)
+        logits = self.translation_layer(x2)
 
-        return x
+        return logits
